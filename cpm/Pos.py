cf293eaab192a8b0a33ff678a390483e4d7bc421
ataro <atr19love5@gmail.com>
2022-10-30 00:47:55 +0700

    123

--- cpm/Pos.py
+++ cpm/Pos.py
@@ -19,10 +19,10 @@ Vheader={
 # for cekdata in ["data"]:
 #     if cekdata not in Vdata:
 #         Vdata[cekdata]=input(f"{cekdata} : ")
+Vdata["idToken"]="eyJhbGciOiJSUzI1NiIsImtpZCI6IjNmNjcyNDYxOTk4YjJiMzMyYWQ4MTY0ZTFiM2JlN2VkYTY4NDZiMzciLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJodHRwczovL3NlY3VyZXRva2VuLmdvb2dsZS5jb20vY3AtbXVsdGlwbGF5ZXIiLCJhdWQiOiJjcC1tdWx0aXBsYXllciIsImF1dGhfdGltZSI6MTY2Njk2NzcxNywidXNlcl9pZCI6InIybnBPekpuaTloWXU1WWZEbWduamZUUVVhSjMiLCJzdWIiOiJyMm5wT3pKbmk5aFl1NVlmRG1nbmpmVFFVYUozIiwiaWF0IjoxNjY2OTY3NzE3LCJleHAiOjE2NjY5NzEzMTcsImVtYWlsIjoidHNiMDEwQGdtYWlsLmNvbSIsImVtYWlsX3ZlcmlmaWVkIjpmYWxzZSwiZmlyZWJhc2UiOnsiaWRlbnRpdGllcyI6eyJlbWFpbCI6WyJ0c2IwMTBAZ21haWwuY29tIl19LCJzaWduX2luX3Byb3ZpZGVyIjoicGFzc3dvcmQifX0.gztM8gzOpNdW6PmmA_hfJDpxh8hCVC1RgAeKcBeVs5-sZnALK3gOtA85ip8khJyhC0N7i0iPwN9TsJqMGvqPb6qHGtuoe9agXWyPdqiztmKEBsJpJrvjPKJj24PmmoREnDnJq8cZHgX7BPyPoD25ZqhCC1pEkjz_XSoQF4qwLhM7-BR8BjcVYkWxxKxJPzfjMc3WfaW2ehHbYEjx8RcOQvsW6yp1zgGXYlbMavbyjHE2Zbl5ameRwic47WD_C6ugH4vtIf7XJWxbAF02CBHicJqLAkCQt0ioxtQpsYu_yq6khnNOA7vqCIn3G8kfmLIZvTNH9kQ-q7X59ePUwgvrmA"
 Vdata["key"]="AIzaSyBW1ZbMiUeDZHYUO2bY8Bfnf5rRgrQGPTM"
-Vdata["firebase-instance-id-token"]="ea9gG_uUWlo:APA91bGw8J3rXLD26czTasH4UqHxJ8wZ6aVn1mKQpHUVs5_wWzh3mkivULK6yNtjKpTolKX4iso4_9xWwXa5AQH9kDTQl04MQCnT1r5C6rSeZeXugaVIo62vjtTyWZpiYcplo2JfVL1n"
+Vdata["firebase-instance-id-token"]="fchcZJLSMpo:APA91bF8nZQY5awRdIgI41tGbAr59K6SuXEeHXC9lQiHcjNR7SN2lD4OKlQ8VuhsgJrF38NgXkDufWoDCXKz-iixYUjeNx7KildcWuQimgagDhWDMxslXhFpaQtujmqn1JywoTEvXVYZ"
 Vdata["data"]="9FD07A11C3494803B517F92545ED5A6702AD0AD2"
-
 
 def verifyPassword(email,password):
     uri=f"https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key={Vdata['key']}"
@@ -32,6 +32,7 @@ def verifyPassword(email,password):
         ress=json.loads(req.text)
         print(f"verifyPassword : {len(ress)}")
         Vdata["idToken"]=ress["idToken"]
+        # print(Vdata["idToken"])
 def getAccountInfo():
     uri=f"https://www.googleapis.com/identitytoolkit/v3/relyingparty/getAccountInfo?key={Vdata['key']}"
     data={"idToken":Vdata["idToken"]}
@@ -54,11 +55,11 @@ def GetPlayerRecords():
     if req.status_code==200:
         ress=json.loads(req.text)
         resss=json.loads(ress["result"])
-        with open('data.json', 'w', encoding='utf-8') as f:
+        with open('player/data.json', 'w', encoding='utf-8') as f:
             json.dump({"data":resss}, f, ensure_ascii=False, indent=4)
         print(f"Data Player : {len(resss)}")
-        print(resss["Name"])
-        print(resss["localID"])
+        print(f' >> Nickname : {resss["Name"]}')
+        print(f' >> ID       : {resss["localID"]}')
 def GetCarHash():
     uri=f"https://{Vhost}/GetCarHash"
     heder={
@@ -74,11 +75,10 @@ def GetCarHash():
     if req.status_code==200:
         ress=json.loads(req.text)
         resss=json.loads(ress["result"])
-        with open('carhash.json', 'w', encoding='utf-8') as f:
+        with open('player/carhash.json', 'w', encoding='utf-8') as f:
             json.dump({"result":resss}, f, ensure_ascii=False, indent=4)
         print(f"Car Hash : {len(resss)}")
 def SavePlayerRecords7(dataakun):
-    print("Save Account Info")
     uri=f"https://{Vhost}/SavePlayerRecords7"
     heder={
         "Host":Vhost,
@@ -95,12 +95,13 @@ def SavePlayerRecords7(dataakun):
     # print(req.text)
     if req.status_code==200:
         ress=json.loads(req.text)
+        print(f"Save Account Info {ress}")
         resss=json.loads(ress["result"])
-        # print(resss)
+        if resss==1:
+            return True
         return True
     return False
 def SaveCarHash(dataakun):
-    print("Save Car Hash")
     uri=f"https://{Vhost}/SaveCarHash"
     heder={
         "Host":Vhost,
@@ -110,7 +111,7 @@ def SaveCarHash(dataakun):
         "accept-encoding":"gzip",
         "User-Agent": f"Dalvik/2.1.0 (Linux; U; Android 8.1.0; ASUS_X00TD MIUI/16.2017.2009.087-20{rdm.randint(111111,999999)})"
     }
-    pipit=json.dumps(dataakun)
+    pipit=json.dumps(dataakun["result"])
     data={"data":pipit}
     req=httpx.post(uri,data=json.dumps(data),headers=heder)
     # print(req.status_code)
@@ -118,12 +119,12 @@ def SaveCarHash(dataakun):
     if req.status_code==200:
         ress=json.loads(req.text)
         resss=json.loads(ress["result"])
-        # print(resss)
-        return True
+        print(f"Save Car Hash : {ress}")
+        if resss==1:
+            return True
     return False
 
 def SaveCars(data):
-    print("Save Cars")
     uri=f"https://{Vhost}/SaveCars"
     heder={
         "Host":Vhost,
@@ -134,21 +135,80 @@ def SaveCars(data):
         "User-Agent": f"Dalvik/2.1.0 (Linux; U; Android 8.1.0; ASUS_X00TD MIUI/16.2017.2009.087-20{rdm.randint(111111,999999)})"
     }
     # pipit=json.dumps(data["data"])
-    data=json.dumps(data)
-    print(json.load(data["data"]))
+    # data=json.dumps(data)
+    data=json.dumps({"data":json.dumps(data["data"])})
     req=httpx.post(uri,data=data,headers=heder)
-    print(req.status_code)
-    print(req.text)
     if req.status_code==200:
         ress=json.loads(req.text)
         resss=json.loads(ress["result"])
-        # print(resss)
         return True
+    print(req.status_code)
+    print(req.text)
     return False
 
+def TestGetAllCars():
+    uri=f"https://{Vhost}/TestGetAllCars"
+    heder={
+        "Host":Vhost,
+        "authorization":f'Bearer {Vdata["idToken"]}',
+        "firebase-instance-id-token":Vdata["firebase-instance-id-token"],
+        "content-type":"application/json; chatset=utf-8",
+        "accept-encoding":"gzip",
+        "User-Agent": f"Dalvik/2.1.0 (Linux; U; Android 8.1.0; ASUS_X00TD MIUI/16.2017.2009.087-20{rdm.randint(111111,999999)})"
+    }
+    data={"data":Vdata["data"]}
+    req=httpx.post(uri,data=json.dumps(data),headers=heder,timeout=1000)
+    if req.status_code==200:
+        ress=json.loads(req.text)
+        resss=json.loads(ress["result"])
+        urutan=""
+        for itr in range(len(resss)):
+            urutan+=str(resss[itr]["CarID"])+","
+            with open(f'player/cars/{resss[itr]["CarID"]}', 'w', encoding='utf-8') as f:
+                json.dump({"data":resss[itr]}, f, ensure_ascii=False, indent=4)
+        print(urutan)
+        print(f"Data : {len(resss)}")
 
 
-
+#World Sale
+def GetCarListWorldSale2(wsvalue):
+    # print("Get Car List WorldSale2")
+    uri=f"https://{Vhost}/GetCarListWorldSale2"
+    heder={
+        "Host":Vhost,
+        "authorization":f'Bearer {Vdata["idToken"]}',
+        "firebase-instance-id-token":Vdata["firebase-instance-id-token"],
+        "content-type":"application/json; chatset=utf-8",
+        "accept-encoding":"gzip",
+        "User-Agent": f"Dalvik/2.1.0 (Linux; U; Android 8.1.0; ASUS_X00TD MIUI/16.2017.2009.087-20{rdm.randint(111111,999999)})"
+    }
+    data=json.dumps({"data":{"@type":"type.googleapis.com/google.protobuf.Int64Value","value":wsvalue}})
+    req=httpx.post(uri,data=data,headers=heder,timeout=10)
+    # print(req.text)
+    if req.status_code==200:
+        ress=json.loads(req.text)
+        return ress["result"]
+def TestGetOneCarFromWorldSale(ownerAccountID,carid,wsvalue):
+    print("Get Car From WorldSale")
+    uri=f"https://{Vhost}/TestGetOneCarFromWorldSale"
+    heder={
+        "Host":Vhost,
+        "authorization":f'Bearer {Vdata["idToken"]}',
+        "firebase-instance-id-token":Vdata["firebase-instance-id-token"],
+        "content-type":"application/json; chatset=utf-8",
+        "accept-encoding":"gzip",
+        "User-Agent": f"Dalvik/2.1.0 (Linux; U; Android 8.1.0; ASUS_X00TD MIUI/16.2017.2009.087-20{rdm.randint(111111,999999)})"
+    }
+    data=json.dumps({"data":[str(ownerAccountID),str(carid),str(wsvalue)]})
+    print(data)
+    req=httpx.post(uri,data=data,headers=heder,timeout=10)
+    print(req.text)
+    if req.status_code==200:
+        ress=json.loads(req.text)
+        return ress["result"]
+        
+        
+        
 
 def signupNewUser(email,password):
     uri=f"https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key={Vdata['key']}"
@@ -160,3 +220,4 @@ def signupNewUser(email,password):
         Vdata["idToken"]=ress["idToken"]
         return True
     return False
+
--- main.py
+++ main.py
@@ -1,16 +1,56 @@
-import os,json,random
+import os,json,random,shutil,time,sys
 import cpm as cpm
 
 
+def cariid(urutan):
+    with open('nomer_car.json', 'r',encoding='utf-8') as openfile: data = json.load(openfile)
+    for mydatacar in data["result"]:
+        if mydatacar["urutan"]==urutan:
+            return mydatacar["id"]
+def cariurutan(cariid):
+    with open('nomer_car.json', 'r',encoding='utf-8') as openfile: data = json.load(openfile)
+    for mydatacar in data["result"]:
+        if mydatacar["id"]==cariid:
+            return mydatacar["urutan"]
+
+
+def cekdatalivery(carid):
+    path = "cpm/cars/livery/"
+    dir_list = os.listdir(path)
+    dir_list=sorted(dir_list, key=len, reverse=False)
+    if str(carid) in dir_list:
+        return True
+    else:return False
+
+def savewscar(carid,data):
+    datacarname=str(carid)
+    itrliverydata=1
+    while True:
+        print(datacarname)
+        if cekdatalivery(datacarname)==False:
+            break
+        else:
+            datacarname=f"{carid}_"+str(itrliverydata)
+            itrliverydata+=1
+                
+        time.sleep(1)
+    print(f"bisa di save {datacarname}")
+    dataforsave={
+        "data":data["thisCarData"]
+    }
+    dataforsave["data"]["Vynils"]=data["thisCarVynils"]
+    print(dataforsave)
+    with open(f'cpm/cars/livery/{datacarname}', 'w', encoding='utf-8') as f:
+        json.dump(dataforsave, f, ensure_ascii=False, indent=4)
+    return datacarname
+
 def tes():
-    with open('request_body.json', 'r') as openfile:
-        dataakun = json.load(openfile)
-    data={"data":json.loads(dataakun["data"])}
-    with open('car13.json', 'w', encoding='utf-8') as f:
-        json.dump(data, f, ensure_ascii=False, indent=4)
-    return data
-# pl=tes()
-# print(pl)
+    datacar=json.dumps(json.loads(datay["result"]),indent=2)
+    datacar=json.loads(datacar)
+    savewscar(datacar["carID"],datacar)
+# while True:
+#     tes()
+#     input()
 # exit()
 
 
@@ -28,11 +68,15 @@ if __name__=="__main__":
 =========================
         Topix SB CPM TOOLS V1.0
 =========================
+0. tes
 1. edit account
 2. create account
 number (x exit): """
         inp=input(menus)
         if inp=="x" or inp=="X":break
+        elif inp=="0":
+            pass
+
         elif inp=="1":
             print("=================== verifyPassword")
             cpm.verifyPassword(input("email : "),input("password : "))
@@ -40,54 +84,198 @@ number (x exit): """
             cpm.getAccountInfo()
             print("=================== GetPlayerRecords")
             cpm.GetPlayerRecords()
+            print("=================== TestGetAllCars")
+            shutil.rmtree('player/cars')
+            os.mkdir('player/cars') 
+            cpm.TestGetAllCars()
             print("=================== GetCarHash")
             cpm.GetCarHash()
             print("=================== SavePlayerRecords7")
             menusedit="""
 =========================
-1. save
+1. manual edit save
 2. inject mod
-3. save cars
+3. change cars to mod/ori
+4. World Sale
+5. edit player cars
+6. unlock klakson
+7. inject livery car
 number (x exit): """
-            with open('data.json', 'r') as openfile:
-                data = json.load(openfile)
-            with open('carhash.json', 'r') as openfile:
-                datacar = json.load(openfile)
+            
             while True:
                 inp=input(menusedit)
                 if inp=="x" or inp=="X":break
                 elif inp=="1":
-                    if cpm.SavePlayerRecords7(data) and cpm.SaveCarHash(datacar):
-                        disp="Sukses"
+                    with open('player/data.json', 'r',encoding='utf-8') as openfile: data = json.load(openfile)
+                    with open('player/carhash.json', 'r',encoding='utf-8') as openfile: datacar = json.load(openfile)
+                    
+                    if cpm.SavePlayerRecords7(data) and cpm.SaveCarHash(datacar): disp="Sukses"
                     else: disp="Gagal"
                 elif inp=="2":
-                    with open('data_mod.json', 'r') as openfile:
-                        datamod = json.load(openfile)
-                    with open('carhash_mod.json', 'r') as openfile:
-                        datacar = json.load(openfile)
-                    # datamod["data"]["allData"]=data["data"]["allData"]
+                    print("=================== GetPlayerRecords")
+                    cpm.GetPlayerRecords()
+                    with open('player/data.json', 'r',encoding='utf-8') as openfile: data = json.load(openfile)
+                    with open('data_mod.json', 'r',encoding='utf-8') as openfile: datamod = json.load(openfile)
+                    with open('carhash_mod.json', 'r',encoding='utf-8') as openfile: datacar = json.load(openfile)
+
+                    datamod["data"]["allData"]=data["data"]["allData"]
                     datamod["data"]["Name"]=data["data"]["Name"]
                     datamod["data"]["localID"]=data["data"]["localID"]
                     datamod["data"]["FriendsID"]=data["data"]["FriendsID"]
-                    datamod["data"]["money"]=data["data"]["money"]
-                    datamod["data"]["coin"]=data["data"]["coin"]
+                    # datamod["data"]["money"]=data["data"]["money"]
+                    # datamod["data"]["coin"]=data["data"]["coin"]
                     data=datamod
-                elif inp=="3":
-                    if input(" 1:ori 2:mod = ")=="2":
-                        tipe="mod"
-                    else:tipe="ori"
-                    with open(f'cpm/cars/{tipe}/13', 'r') as openfile:
-                        datacar = json.load(openfile)
-                    if cpm.SaveCars(datacar):
-                        disp="Sukses"
+
+                    with open('player/data.json', 'w', encoding='utf-8') as f:
+                        json.dump(data, f, ensure_ascii=False, indent=4)
+                    with open('player/carhash.json', 'w', encoding='utf-8') as f:
+                        json.dump(datacar, f, ensure_ascii=False, indent=4)
+
+                    if cpm.SavePlayerRecords7(data) and cpm.SaveCarHash(datacar): disp="Sukses"
                     else: disp="Gagal"
+                    
+                    path = "cpm/cars/mod/"
+                    dir_list = os.listdir(path)
+                    print("Files and directories in '", path, "' :")
+                    dir_list=sorted(dir_list, key=len, reverse=False)
+                    for idcar in dir_list:
+                        print(f"  >> Inject Car id [{idcar}]")
+                        with open(f'cpm/cars/mod/{idcar}', 'r',encoding='utf-8') as openfile: datacar = json.load(openfile)
+                        if cpm.SaveCars(datacar): disp="Sukses"
+                        else: disp="Gagal"
+                elif inp=="3":
+                    if input(" 1:ori 2:mod = ")=="2": tipe="mod"
+                    else:tipe="ori"
+                    idcar=input("ID CAR : ")
+                    with open(f'cpm/cars/{tipe}/{idcar}', 'r',encoding='utf-8') as openfile: datacar = json.load(openfile)
+                    if cpm.SaveCars(datacar): disp="Sukses"
+                    else: disp="Gagal"
+                elif inp=="4":
+                    print()
+                    autosep=False
+                    if input("auto save [y/n] : ").lower()=="y":
+                        autosep=True
+                    wsvalue=input("World Value : ")
+                    while True:
+                        xpo=cpm.GetCarListWorldSale2(wsvalue)
+                        if xpo != None:
+                            klp=xpo.strip('][').split('},{')
+                            idxsale=0
+                            if len(klp)>1:
+                                for tyy in klp:
+                                    print(f"{idxsale} - {len(klp)}")
+                                    if idxsale==0:
+                                        aitem=tyy+"}"
+                                    elif idxsale==len(klp)-1:
+                                        aitem="{"+tyy[:-1]
+                                    else:
+                                        aitem="{"+tyy+"}"
+                                    try:
+                                        caitem=json.loads(aitem)
+                                        print("<<------------------------------------>>")
+                                        for itemv in caitem:
+                                            print(f"{itemv}\t:{caitem[itemv]}")
+                                        idxsale+=1
+                                        carxpo=cpm.TestGetOneCarFromWorldSale(caitem["ownerAccountID"],caitem["carID"],wsvalue)
+                                        datacar=json.dumps(json.loads(carxpo),indent=2)
+                                        datacar=json.loads(datacar)
+                                        print(f'OwnerID : {datacar["ownerID"]}')
+                                        print(f'Name    : {datacar["ownerName"]}')
+                                        print(f'Car id  : {datacar["carID"]}')
+                                        print(f'Price   : {datacar["price"]}')
+                                        print(f'Desc    : {datacar["description"]}')
+                                        datacarname=savewscar(datacar["carID"],datacar)
+                                        print("<<------------------------------------>>")
+                                        print()
+                                        if autosep:
+                                            print("> Save To Account")
+                                            with open(f'cpm/cars/livery/{datacarname}', 'r',encoding='utf-8') as openfile: datacar = json.load(openfile)
+                                            datacar["data"]["floats"][0]=1
+                                            if cpm.SaveCars(datacar): disp="Sukses"
+                                            else: disp="Gagal"
+                                    except:
+                                        print(f"gagal ambil item : {aitem}")
+                            else:
+                                pass
+                                # print(f"Len klp : {len(klp)}")
+                        for tunggu in range(60,0,-1):
+                            sys.stdout.write(f" wait {tunggu} \r")
+                            sys.stdout.flush()
+                            time.sleep(1)
+                elif inp=="5":
+                    dir_list=sorted(os.listdir("cpm/cars/livery/"), key=len, reverse=False)
+                    itrlivery=1
+                    print("\n  [ Urutan Livery yang tersedia ]")
+                    for idnya in dir_list:
+                        if "_" not in idnya:
+                            print(f"  {itrlivery}. {cariurutan(idnya)}")
+                        itrlivery+=1
+                    urutcar=input("\nurutan car : ")
+                    idcar=cariid(int(urutcar))
+                    if idcar==None:
+                        print("ga ada data id car")
+                    else:
+                        print(f"ID CAR NYA ADALAH {idcar}")
+                        with open(f'player/cars/{idcar}', 'r',encoding='utf-8') as openfile: datacar = json.load(openfile)
+                        menuscar="""
+                
+     [ Menu Edit ]
+x to save
+1. Police on
+2. Police off
+3. Timpa pakai livery
+choice : 
+"""
+                        
+                            
+                        while True:
+                            caredit=input(menuscar)
+                            if caredit=="x":break
+                            elif caredit=="1":
+                                datacar["data"]["floats"][0]=1
+                            elif caredit=="2":
+                                datacar["data"]["floats"][0]=0
+                            elif caredit=="3":
+                                try:
+                                    with open(f'cpm/cars/livery/{input("nama file di folder livery : ")}', 'r',encoding='utf-8') as openfile: datacar = json.load(openfile)
+                                except:
+                                    print(f"Belum punya data livery mobil ke {urutcar}")
+
+                        if cpm.SaveCars(datacar): disp="Sukses"
+                        else: disp="Gagal"
+                elif inp=="6":
+                    print("=================== GetPlayerRecords")
+                    cpm.GetPlayerRecords()
+                    with open('player/data.json', 'r',encoding='utf-8') as openfile: data = json.load(openfile)
+                    data["data"]["floats"][27]=1
+                    data["data"]["floats"][28]=1
+                    data["data"]["floats"][29]=1
+                    data["data"]["floats"][30]=1
+                    data["data"]["floats"][31]=1
+
+                    if cpm.SavePlayerRecords7(data): disp="Sukses"
+                    else: disp="Gagal"
+                elif inp=="7":
+                    dir_list=os.listdir("cpm/cars/livery/")
+                    dir_list.sort(key=int)
+                    for idcar in dir_list:
+                        if "_" not in idcar:
+                            try:
+                                print(idcar)
+                                with open(f'cpm/cars/livery/{idcar}', 'r',encoding='utf-8') as openfile: datacar = json.load(openfile)
+                                datacar["data"]["floats"][0]=1
+                                if cpm.SaveCars(datacar): disp="Sukses"
+                                else: disp="Gagal"
+                            except:
+                                print(f"Belum punya data livery mobil ke {urutcar}")
+
         elif inp=="2":
             print("Create Account")
             if cpm.signupNewUser(input(" Email : "),input(" Password : ")):
-                with open('basedata.json', 'r') as openfile:
+                with open('data_basic.json', 'r') as openfile:
                     data = json.load(openfile)
                 data["data"]["localID"]="TSB"+str(random.randint(11111,99999))
-                with open('basecarhash.json', 'r') as openfile:
+                with open('carhash_basic.json', 'r') as openfile:
                     datacar = json.load(openfile)
                 if cpm.SavePlayerRecords7(data) and cpm.SaveCarHash(datacar):
                     disp="Sukses"
